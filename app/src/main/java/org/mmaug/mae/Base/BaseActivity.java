0e77eef2482edd7d254a399396418b4c1baecdb7
indexer <yemonkyaw007@gmail.com>
2015-09-13 06:22:09 +0630

    added BaseActivity

---
+++ app/src/main/java/org/mmaug/mae/Base/BaseActivity.java
@@ -0,0 +1,14 @@
+package org.mmaug.mae.Base;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by indexer on 9/13/15.
+ */
+public class BaseActivity extends AppCompatActivity {
+  @Override public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+    super.onCreate(savedInstanceState, persistentState);
+  }
+}
--- app/src/main/java/org/mmaug/mae/MainActivity.java
+++ app/src/main/java/org/mmaug/mae/MainActivity.java
@@ -1,11 +1,11 @@
 package org.mmaug.mae;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import org.mmaug.mae.Base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
