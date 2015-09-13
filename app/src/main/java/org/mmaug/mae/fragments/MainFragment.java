0aa8ea9f9328eddeade8f7629794c0a8ab6faba2
indexer <yemonkyaw007@gmail.com>
2015-09-13 09:39:15 +0630

    date picker added and check dob is 18

--- app/src/main/java/org/mmaug/mae/fragments/MainFragment.java
+++ app/src/main/java/org/mmaug/mae/fragments/MainFragment.java
@@ -7,8 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import java.util.Calendar;
 import org.mmaug.mae.R;
 import org.mmaug.mae.adapter.AvatarAdapter;
 import org.mmaug.mae.models.Avatar;
@@ -16,16 +19,20 @@ import org.mmaug.mae.models.Avatar;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment
+    implements com.wdullaer.materialdatetimepicker.date.DatePickerDialog.OnDateSetListener {
 
+  @Bind(R.id.avatars) GridView mAvatarGrid;
+  @Bind(R.id.date_of_birth) TextView mDateOfBirth;
+  Calendar now;
+  int maxAgeforVote = 18;
   private Avatar mSelectedAvatar = Avatar.ONE;
   private View mSelectedAvatarView;
-  @Bind(R.id.avatars) GridView mAvatarGrid;
 
   public MainFragment() {
   }
 
-  private void setUpGridView(View container) {
+  private void setUpGridView() {
     mAvatarGrid.setAdapter(new AvatarAdapter(getActivity()));
     mAvatarGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
       @Override public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -35,6 +42,15 @@ public class MainFragment extends Fragment {
     });
     mAvatarGrid.setNumColumns(4);
     mAvatarGrid.setItemChecked(mSelectedAvatar.ordinal(), true);
+  }
+
+  @OnClick(R.id.date_of_birth) void DatePicker() {
+    now = Calendar.getInstance();
+    com.wdullaer.materialdatetimepicker.date.DatePickerDialog datePickerDialog =
+        com.wdullaer.materialdatetimepicker.date.DatePickerDialog.newInstance(this,
+            now.get(Calendar.YEAR) - maxAgeforVote, now.get(Calendar.MONTH),
+            now.get(Calendar.DAY_OF_MONTH));
+    datePickerDialog.show(getActivity().getFragmentManager(), "Datepickerdialog");
   }
 
   //TODO reenable
@@ -47,8 +63,17 @@ public class MainFragment extends Fragment {
   @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-    ButterKnife.bind(this,rootView);
-    setUpGridView(rootView);
+    ButterKnife.bind(this, rootView);
+    setUpGridView();
     return rootView;
+  }
+
+  @Override
+  public void onDateSet(com.wdullaer.materialdatetimepicker.date.DatePickerDialog view, int year,
+      int monthOfYear, int dayOfMonth) {
+    now = Calendar.getInstance();
+    if ((now.get(Calendar.YEAR) - year) >= 18) {
+      mDateOfBirth.setText(year + "-" + monthOfYear + "-" + dayOfMonth);
+    }
   }
 }
