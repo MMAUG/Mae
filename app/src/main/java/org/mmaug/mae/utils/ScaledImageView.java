7762bd57d30f588a02faac777dc05061a5af4ac1
indexer <yemonkyaw007@gmail.com>
2015-09-20 02:08:38 +0630

    image widh and height aspect ratio fixed

---
+++ app/src/main/java/org/mmaug/mae/utils/ScaledImageView.java
@@ -0,0 +1,35 @@
+package org.mmaug.mae.utils;
+
+/**
+ * Created by indexer on 9/20/15.
+ */
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class ScaledImageView extends ImageView {
+  public ScaledImageView(final Context context, final AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  @Override protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+    final Drawable d = getDrawable();
+
+    if (d != null) {
+      int width;
+      int height;
+      if (MeasureSpec.getMode(heightMeasureSpec) == MeasureSpec.EXACTLY) {
+        height = MeasureSpec.getSize(heightMeasureSpec);
+        width = (int) Math.ceil(height * (float) d.getIntrinsicWidth() / d.getIntrinsicHeight());
+      } else {
+        width = MeasureSpec.getSize(widthMeasureSpec);
+        height = (int) Math.ceil(width * (float) d.getIntrinsicHeight() / d.getIntrinsicWidth());
+      }
+      setMeasuredDimension(width, height);
+    } else {
+      super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+  }
+}
