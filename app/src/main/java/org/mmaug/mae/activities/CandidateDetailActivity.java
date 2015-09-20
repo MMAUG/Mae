3239ad531eff69c73c4d77b89094a22924ba06b7
indexer <yemonkyaw007@gmail.com>
2015-09-20 15:54:44 +0630

    added circle transformation in party detail

--- app/src/main/java/org/mmaug/mae/activities/CandidateDetailActivity.java
+++ app/src/main/java/org/mmaug/mae/activities/CandidateDetailActivity.java
@@ -2,33 +2,27 @@ package org.mmaug.mae.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.bumptech.glide.Glide;
 import jp.wasabeef.glide.transformations.BlurTransformation;
-import jp.wasabeef.glide.transformations.CropCircleTransformation;
 import org.mmaug.mae.R;
 import org.mmaug.mae.models.Party;
 import org.mmaug.mae.view.ZoomAspectRatioImageView;
 
-public class PartyDetailActivity extends AppCompatActivity {
-  @Bind(R.id.backdrop) ZoomAspectRatioImageView party_image;
-  @Bind(R.id.candidate_image) ImageView candidate;
+public class CandidateDetailActivity extends AppCompatActivity {
+  @Bind(R.id.party_image_demo) ZoomAspectRatioImageView party_image;
+  @Bind(R.id.container) RelativeLayout container;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_candidate_detail);
+    setContentView(R.layout.activity_party_detail);
     ButterKnife.bind(this);
-
     Party party = (Party) getIntent().getSerializableExtra("party");
     Glide.with(this)
         .load(party.getPartyFlag())
         .bitmapTransform(new BlurTransformation(getApplicationContext(), 8, 1))
         .into(party_image);
-    Glide.with(this)
-        .load(R.drawable.candidate)
-        .bitmapTransform(new CropCircleTransformation(this))
-        .into(candidate);
   }
 }
