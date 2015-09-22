fc37566029d1ae88de43c695830a5f8f06c705d1
Poe Poe <poepoe88@gmail.com>
2015-09-23 02:12:31 +0800

    fix (candidate): sort candidate list and change birthdate to long

--- app/src/main/java/org/mmaug/mae/rest/RESTClient.java
+++ app/src/main/java/org/mmaug/mae/rest/RESTClient.java
@@ -69,13 +69,13 @@ public class RESTClient {
 
       long t1 = System.nanoTime();
       Log.i(TAG, String.format("Sending request %s on %s%n%s", request.url(), chain.connection(),
-          request.headers()));
+          request.body()));
 
       Response response = chain.proceed(request);
 
       long t2 = System.nanoTime();
       Log.i(TAG, String.format("Received response for %s in %.1fms%n%s", response.request().url(),
-          (t2 - t1) / 1e6d, response.headers()));
+          (t2 - t1) / 1e6d, response.body()));
 
       return response;
     }
