2bdfeeb866ddd0c112d678d816d6b0d6854be97b
Ye Myat Thu <thu.yemyat@gmail.com>
2015-09-25 01:09:17 +0630

    Update Party Detail

--- app/src/main/java/org/mmaug/mae/activities/PartyDetailActivity.java
+++ app/src/main/java/org/mmaug/mae/activities/PartyDetailActivity.java
@@ -1,17 +1,21 @@
 package org.mmaug.mae.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.bumptech.glide.Glide;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import org.mmaug.mae.Config;
 import org.mmaug.mae.R;
+import org.mmaug.mae.adapter.ToyFigurePagerAdapter;
 import org.mmaug.mae.models.Party;
 import org.mmaug.mae.utils.MixUtils;
 
@@ -24,8 +28,10 @@ public class PartyDetailActivity extends AppCompatActivity {
   @Bind(R.id.constituency)TextView mConstituency;
   @Bind(R.id.party_headquarter) TextView mPartyHeadQuarter;
   @Bind(R.id.party_leader) TextView mPartyLeader;
-  @Bind(R.id.party_phone) TextView mPartyPhone;
-  @Bind(R.id.this_year_toys) LinearLayout mThisYearToys;
+  @Bind(R.id.current_year_tab) TabLayout mCurrentTabLayout;
+  @Bind(R.id.current_year_view_pager) ViewPager mCurrentViewPager;
+  @Bind(R.id.prev_year_tab) TabLayout mPrevTabLayout;
+  @Bind(R.id.prev_year_view_pager) ViewPager mPrevViewPager;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -60,8 +66,30 @@ public class PartyDetailActivity extends AppCompatActivity {
         mPartyLeader.append(contact+"\n");
       }
     }
+    ToyFigurePagerAdapter currentPagerAdapter = new ToyFigurePagerAdapter();
+    Map<String,Integer> currentCandidateCount = new HashMap<>();
+    currentCandidateCount.put(Config.AMYOTHAE_HLUTTAW,5);
+    currentCandidateCount.put(Config.PYITHU_HLUTTAW,3);
+    currentCandidateCount.put(Config.TINEDAYTHA_HLUTTAW, 6);
+    currentPagerAdapter.setItems(currentCandidateCount);
+    mCurrentViewPager.setAdapter(currentPagerAdapter);
+    mCurrentViewPager.setCurrentItem(0);
+    mCurrentTabLayout.setTabMode(TabLayout.MODE_FIXED);
+    mCurrentTabLayout.setupWithViewPager(mCurrentViewPager);
+
+    ToyFigurePagerAdapter prevPagerAdapter = new ToyFigurePagerAdapter();
+    mPrevViewPager.setAdapter(prevPagerAdapter);
+    Map<String,Integer> prevCandidateCount = new HashMap<>();
+    prevCandidateCount.put(Config.AMYOTHAE_HLUTTAW,7);
+    prevCandidateCount.put(Config.PYITHU_HLUTTAW,5);
+    prevCandidateCount.put(Config.TINEDAYTHA_HLUTTAW, 4);
+    prevPagerAdapter.setItems(prevCandidateCount);
+    mPrevViewPager.setCurrentItem(0);
+    mPrevTabLayout.setTabMode(TabLayout.MODE_FIXED);
+    mPrevTabLayout.setupWithViewPager(mPrevViewPager);
+
     // TODO: 9/24/15 REMOVE HARDCODED VALUE
-    colorFilterImageViews(mThisYearToys,1400/400);
+    //colorFilterImageViews(mThisYearToys,1400/400);
   }
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
@@ -70,15 +98,5 @@ public class PartyDetailActivity extends AppCompatActivity {
       return true;
     }
     return false;
-  }
-
-  private void colorFilterImageViews(ViewGroup parent,int countToColor){
-    for(int i=0;i<parent.getChildCount();i++){
-      if(i<countToColor-1){
-        ((ImageView)parent.getChildAt(i)).setColorFilter(getResources().getColor(R.color.red));
-      }else{
-        ((ImageView)parent.getChildAt(i)).setColorFilter(getResources().getColor(R.color.mdtp_light_gray));
-      }
-    }
   }
 }
