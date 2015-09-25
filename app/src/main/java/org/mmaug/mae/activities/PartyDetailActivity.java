a7552ef30308be864d117bef3bf055df868289a7
Ye Myat Thu <thu.yemyat@gmail.com>
2015-09-25 14:22:36 +0630

    Party Detail Implement

--- app/src/main/java/org/mmaug/mae/activities/PartyDetailActivity.java
+++ app/src/main/java/org/mmaug/mae/activities/PartyDetailActivity.java
@@ -10,14 +10,23 @@ import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.mmaug.mae.Config;
 import org.mmaug.mae.R;
 import org.mmaug.mae.adapter.ToyFigurePagerAdapter;
+import org.mmaug.mae.models.CurrentCollection;
 import org.mmaug.mae.models.Party;
+import org.mmaug.mae.rest.RESTClient;
+import org.mmaug.mae.rest.RESTService;
 import org.mmaug.mae.utils.MixUtils;
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.Response;
 
 public class PartyDetailActivity extends AppCompatActivity {
   @Bind(R.id.backdrop) ImageView party_image;
@@ -32,6 +41,13 @@ public class PartyDetailActivity extends AppCompatActivity {
   @Bind(R.id.current_year_view_pager) ViewPager mCurrentViewPager;
   @Bind(R.id.prev_year_tab) TabLayout mPrevTabLayout;
   @Bind(R.id.prev_year_view_pager) ViewPager mPrevViewPager;
+
+  private RESTService partyDetailRestService;
+  private int currentAmyotharCount;
+  private int currentPyithuHlutaw;
+  private int currentTineDaythaHlutaw;
+  private CurrentCollection mAmyothaCurrentCollection;
+  private CurrentCollection mPyithuCurrentCollection;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -66,12 +82,7 @@ public class PartyDetailActivity extends AppCompatActivity {
         mPartyLeader.append(contact + "\n");
       }
     }
-    ToyFigurePagerAdapter currentPagerAdapter = new ToyFigurePagerAdapter();
-    Map<String,Integer> currentCandidateCount = new HashMap<>();
-    currentCandidateCount.put(Config.AMYOTHAE_HLUTTAW,5);
-    currentCandidateCount.put(Config.PYITHU_HLUTTAW,3);
-    currentCandidateCount.put(Config.TINEDAYTHA_HLUTTAW, 6);
-    currentPagerAdapter.setItems(currentCandidateCount);
+    final ToyFigurePagerAdapter currentPagerAdapter = new ToyFigurePagerAdapter();
     mCurrentViewPager.setAdapter(currentPagerAdapter);
     mCurrentViewPager.setCurrentItem(0);
     mCurrentTabLayout.setTabMode(TabLayout.MODE_FIXED);
@@ -87,7 +98,40 @@ public class PartyDetailActivity extends AppCompatActivity {
     mPrevViewPager.setCurrentItem(0);
     mPrevTabLayout.setTabMode(TabLayout.MODE_FIXED);
     mPrevTabLayout.setupWithViewPager(mPrevViewPager);
+    partyDetailRestService = RESTClient.getService();
+    Call<JsonObject> candidateCountCall = partyDetailRestService.getCandidateCount(party.getPartyId());
+    final Call<JsonObject> currentCount = partyDetailRestService.getCurrentCount();
+    candidateCountCall.enqueue(new Callback<JsonObject>() {
+      @Override public void onResponse(Response<JsonObject> response) {
+        if(response.isSuccess()){
+          currentAmyotharCount = response.body().get("upper_house").getAsInt();
+          currentPyithuHlutaw = response.body().get("lower_house").getAsInt();
+          currentTineDaythaHlutaw = response.body().get("state_region").getAsInt();
+          currentCount.enqueue(new Callback<JsonObject>() {
+            @Override public void onResponse(Response<JsonObject> response) {
+              if(response.isSuccess()){
+                Gson gson = new GsonBuilder().create();
+                mAmyothaCurrentCollection = gson.fromJson(response.body().get("amyotha").getAsJsonObject(),CurrentCollection.class);
+                mPyithuCurrentCollection = gson.fromJson(response.body().get("pyithu").getAsJsonObject(),CurrentCollection.class);
+                Map<String,Integer> currentCandidateCount = new HashMap<>();
+                currentCandidateCount.put(Config.AMYOTHAE_HLUTTAW,mAmyothaCurrentCollection.getSeats()/currentAmyotharCount);
+                currentCandidateCount.put(Config.PYITHU_HLUTTAW,mPyithuCurrentCollection.getSeats()/currentPyithuHlutaw);
+                currentCandidateCount.put(Config.TINEDAYTHA_HLUTTAW,9);
+                currentPagerAdapter.setItems(currentCandidateCount);
+              }
+            }
 
+            @Override public void onFailure(Throwable t) {
+
+            }
+          });
+        }
+      }
+
+      @Override public void onFailure(Throwable t) {
+
+      }
+    });
   }
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
---
+++ app/src/main/java/org/mmaug/mae/models/CurrentCollection.java
@@ -0,0 +1,93 @@
+package org.mmaug.mae.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CurrentCollection {
+
+@SerializedName("Speaker")
+@Expose
+private String Speaker;
+@SerializedName("Deputy Speaker")
+@Expose
+private String DeputySpeaker;
+@SerializedName("Seats")
+@Expose
+private Integer Seats;
+@SerializedName("members")
+@Expose
+private Members members;
+
+/**
+* 
+* @return
+* The Speaker
+*/
+public String getSpeaker() {
+return Speaker;
+}
+
+/**
+* 
+* @param Speaker
+* The Speaker
+*/
+public void setSpeaker(String Speaker) {
+this.Speaker = Speaker;
+}
+
+/**
+* 
+* @return
+* The DeputySpeaker
+*/
+public String getDeputySpeaker() {
+return DeputySpeaker;
+}
+
+/**
+* 
+* @param DeputySpeaker
+* The Deputy Speaker
+*/
+public void setDeputySpeaker(String DeputySpeaker) {
+this.DeputySpeaker = DeputySpeaker;
+}
+
+/**
+* 
+* @return
+* The Seats
+*/
+public Integer getSeats() {
+return Seats;
+}
+
+/**
+* 
+* @param Seats
+* The Seats
+*/
+public void setSeats(Integer Seats) {
+this.Seats = Seats;
+}
+
+/**
+* 
+* @return
+* The members
+*/
+public Members getMembers() {
+return members;
+}
+
+/**
+* 
+* @param members
+* The members
+*/
+public void setMembers(Members members) {
+this.members = members;
+}
+
+}
