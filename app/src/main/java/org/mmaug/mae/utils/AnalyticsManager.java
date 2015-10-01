b88c914272f4ec9e9e8c4d338ba8d3aa0d3ee9c1
Poe Poe <poepoe88@gmail.com>
2015-10-02 03:51:08 +0800

    chores (analytics): added google analytics

---
+++ app/src/main/java/org/mmaug/mae/utils/AnalyticsManager.java
@@ -0,0 +1,58 @@
+package org.mmaug.mae.utils;
+
+import android.content.Context;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+import org.mmaug.mae.R;
+
+public class AnalyticsManager {
+  private static Context sAppContext = null;
+  private static Tracker mTracker;
+
+  public static void sendScreenView(String screenName) {
+    if (canSend()) {
+      mTracker.setScreenName(screenName);
+      mTracker.send(new HitBuilders.AppViewBuilder().build());
+    }
+  }
+
+  private static boolean canSend() {
+    return sAppContext != null && mTracker != null;
+  }
+
+  public static void sendEvent(String category, String action, String label) {
+    sendEvent(category, action, label, 0);
+  }
+
+  public static void sendEvent(String category, String action, String label, long value) {
+    if (canSend()) {
+      mTracker.send(new HitBuilders.EventBuilder().setCategory(category)
+          .setAction(action)
+          .setLabel(label)
+          .setValue(value)
+          .build());
+    }
+  }
+
+  public static synchronized void initializeAnalyticsTracker(Context context) {
+    sAppContext = context;
+    if (mTracker == null) {
+      int useProfile;
+      useProfile = R.xml.analytics_release;
+      mTracker = GoogleAnalytics.getInstance(context).newTracker(useProfile);
+
+      mTracker.enableExceptionReporting(true);
+      mTracker.enableAdvertisingIdCollection(true);
+      mTracker.enableAutoActivityTracking(true);
+    }
+  }
+
+  public Tracker getTracker() {
+    return mTracker;
+  }
+
+  public static synchronized void setTracker(Tracker tracker) {
+    mTracker = tracker;
+  }
+}
