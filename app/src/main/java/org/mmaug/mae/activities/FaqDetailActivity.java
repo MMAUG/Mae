64e76c37134b582fd2dc4ae99141396d6fe9aaf5
Poe Poe <poepoe88@gmail.com>
2015-10-02 00:51:29 +0800

    feat (text): force unicode in faq detail page

--- app/src/main/java/org/mmaug/mae/activities/FaqDetailActivity.java
+++ app/src/main/java/org/mmaug/mae/activities/FaqDetailActivity.java
@@ -1,54 +1,56 @@
 package org.mmaug.mae.activities;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import org.mmaug.mae.R;
+import org.mmaug.mae.base.BaseActivity;
 import org.mmaug.mae.models.FAQ;
-import org.mmaug.mae.utils.FontCache;
+import org.mmaug.mae.utils.MMTextUtils;
 
 /**
  * Created by yemyatthu on 8/6/15.
  */
-public class FaqDetailActivity extends AppCompatActivity {
+public class FaqDetailActivity extends BaseActivity {
   public static final String FAQ_CONSTANT = "org.maepaesoh.maepaesoh.ui.FaqDetailActivity";
 
-  // Ui elements
-  private Toolbar mToolbar;
-  private View mToolbarShadow;
-  private TextView mQuestion;
-  private TextView mAnswer;
-
-  private FAQ mFAQ;
+  @Bind(R.id.faq_question) TextView mQuestion;
+  @Bind(R.id.faq_answer) TextView mAnswer;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_faq_detail);
-    mToolbar = (Toolbar) findViewById(R.id.faq_detail_toolbar);
+    ButterKnife.bind(this);
 
-    mQuestion = (TextView) findViewById(R.id.faq_question);
-    mAnswer = (TextView) findViewById(R.id.faq_answer);
-    setSupportActionBar(mToolbar);
-    ActionBar mActionBar = getSupportActionBar();
-    if (mActionBar != null) {
-      // Showing Back Arrow  <-
-      mActionBar.setDisplayHomeAsUpEnabled(true);
-    }
-    mFAQ = (FAQ) getIntent().getSerializableExtra(FAQ_CONSTANT);
+    FAQ mFAQ = (FAQ) getIntent().getSerializableExtra(FAQ_CONSTANT);
     if (mFAQ != null) {
-      mActionBar.setTitle(mFAQ.getQuestion());
-      Typeface typefaceTitle = FontCache.get("MyanmarAngoun.ttf", this);
-      Typeface typefacelight = FontCache.get("pyidaungsu.ttf", this);
+      setToolbarTitle(mFAQ.getQuestion());
       mQuestion.setText(mFAQ.getQuestion());
       mAnswer.setText(mFAQ.getAnswer());
-      mQuestion.setTypeface(typefaceTitle);
-      mAnswer.setTypeface(typefacelight);
+      if (isUnicode()) {
+        mQuestion.setTypeface(getTypefaceTitle());
+        mAnswer.setTypeface(getTypefaceLight());
+      } else {
+        MMTextUtils.getInstance(this).prepareMultipleViews(mQuestion, mAnswer);
+      }
     }
+  }
+
+  @Override protected int getLayoutResource() {
+    return R.layout.activity_faq_detail;
+  }
+
+  @Override protected boolean getHomeUpEnabled() {
+    return true;
+  }
+
+  @Override protected boolean needToolbar() {
+    return true;
+  }
+
+  @Override protected String getToolbarText() {
+    return "FAQ";
   }
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
