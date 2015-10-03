c5ad346ed496a8ec55c835211b197e54c903271e
Ye Myat Thu <thu.yemyat@gmail.com>
2015-10-03 14:50:47 +0630

    Fix crash on candidate list

--- app/src/main/java/org/mmaug/mae/MaeApp.java
+++ app/src/main/java/org/mmaug/mae/MaeApp.java
@@ -32,20 +32,20 @@ public class MaeApp extends Application {
   private static class CrashReportingTree extends Timber.HollowTree {
 
     @Override public void i(String message, Object... args) {
-      Crashlytics.log(String.format(message, args));
+      //Crashlytics.log(String.format(message, args));
     }
 
     @Override public void i(Throwable t, String message, Object... args) {
-      i(message, args); // Just add to the log.
+      //i(message, args); // Just add to the log.
     }
 
     @Override public void e(String message, Object... args) {
-      i("ERROR: " + message, args); // Just add to the log.
+      //i("ERROR: " + message, args); // Just add to the log.
     }
 
     @Override public void e(Throwable t, String message, Object... args) {
-      e(message, args);
-      Crashlytics.logException(t);
+      //e(message, args);
+      //Crashlytics.logException(t);
     }
   }
 }
